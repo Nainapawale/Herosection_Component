0b901d5fa24c343b132956ceb8c5fe7995114b9d
Nainapawale <nainapawale03@gmail.com>
2025-03-10 14:51:27 +0530

    Initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Row, Col, Button, Container } from "react-bootstrap";
+import "./Hero.css";
+import BGimg from "../assets/background.jpg";
+
+function Hero() {
+  return (
+    <div className="herosection">
+      <Container>
+        <Row className="align-items-center text-center">
+          <Col md={6}>
+            <h1>Hi, I am Naina Pawale, a Web Developer</h1>
+            <p>Building modern and responsive web applications</p>
+            <Button className="button-custom"href="https://github.com/Nainapawale
+            ">
+              View my Work
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Hero;
